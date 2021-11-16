6ef2a19d015ac16dc87555a023e3dba233f1a81d
Bazubagira <bernabazubagira@gmail.com>
2021-11-16 07:10:48 -0800

    Completed file

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-if chr(i) != 'e' and chr(i) != 'q':
-print("{}".format(chr(i)), end='')
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
+        
 
         
