7aa22111db78197f9e11924d9face899ec7d58a2
Bazubagira <bernabazubagira@gmail.com>
2021-11-21 00:24:40 -0800

    replace without modify original

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new = list(my_list)
+    if idx < 0 0r idx > len(my_list) - 1:
+        return (new)
+    else:
+        new[idx] = element
+        return(new)
+    
