00236e86118bb68b8ff907590658731167fc4832
Bazubagira <bernabazubagira@gmail.com>
2021-12-04 08:34:43 -0800

    python more datastructures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    """replace all occurences of an element"""
-new_list = my_list[:]
-new_list = [x if x != search else replace for x in my_list]
-  return (new_list)
-
-    
+    if my_list is not None:
+        return([x if x != search else replace for x in my_list])
+    return None 
