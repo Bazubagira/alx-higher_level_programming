e3ebac32d7ee517a470fd83899e10e93f185514b
Bazubagira <bernabazubagira@gmail.com>
2021-12-04 08:55:10 -0800

    python more datastructures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    """ add all unique integers in a list """
-    result = 0
-    for x in set(my_list):
-        result += x
-        return (result)
-    
+    total = 0
+    for i in set(my_list):
+        total += i
+    return(total)
+
+# return (sum(set(my_list)))
